dc7a280bc80ba70861f86b7869fe5b21b7eb1c6a
LydiaEbikumar <lydiaebikumar@gmail.com>
2024-07-22 18:05:16 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,26 @@
+function text1(){
+    document.getElementById("red01").style.backgroundColor="red";
+    document.getElementById("red1").style.backgroundColor="red";
+}
+function text2(){
+    document.getElementById("red02").style.backgroundColor="red";
+    document.getElementById("red2").style.backgroundColor="red";
+
+
+
+}
+function text3(){
+    document.getElementById("red03").style.backgroundColor="red";
+    document.getElementById("red3").style.backgroundColor="red";
+
+
+
+}
+function text(){
+document.getElementById("green01").style.backgroundColor="green";
+document.getElementById("green1").style.backgroundColor="green";
+
+
+
+}
+
